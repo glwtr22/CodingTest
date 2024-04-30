9b29a8aa8b96f7047e7c79d2d9cdf4e6794dfbcf
김조현 <kimjohyun298@gmail.com>
2024-04-30 13:51:25 +0900

    2096. 내려가기, 6198. 옥상 정원 꾸미기

---
+++ 스터디/백준/0. 유형 모르고 풀기/2096. 내려가기.py
@@ -0,0 +1,67 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+max_dp = [0] * 3
+min_dp = [0] * 3
+
+max_tmp = [0] * 3
+min_tmp = [0] * 3
+
+for i in range(n):
+    a, b, c = map(int, input().split())
+    for j in range(3):
+        if j == 0:
+            max_tmp[j] = a + max(max_dp[j], max_dp[j+1])
+            min_tmp[j] = a + min(min_dp[j], min_dp[j+1])
+        elif j == 1:
+            max_tmp[j] = b + max(max_dp[j-1], max_dp[j], max_dp[j+1])
+            min_tmp[j] = b + min(max_dp[j-1], max_dp[j], max_dp[j+1])
+        else:
+            max_tmp[j] = c + max(max_dp[j], max_dp[j-1])
+            min_tmp[j] = c + min(min_dp[j], min_dp[j-1])
+
+    for j in range(3):
+        max_dp[j] = max_tmp[j]
+        min_dp[j] = min_tmp[j]
+
+
+print(max(max_dp), min(min_dp))
+
+
+'''
+전형적인 DP 문제를 자꾸 백트래킹 문제라고 생각하는 경향이 있음
+코드 :  https://kyun2da.github.io/2021/04/27/goDown/
+
+메모이제이션 : 필요한 만큼의 값만 저장하면서 갱신해나가는 알고리즘
+'''
+
+
+
+# import sys
+# input = sys.stdin.readline
+#
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+#
+# maxSum = 0
+# minSum = int(1e9)
+#
+#
+# def findMax(depth, idx):
+#     global maxSum
+#
+#     if depth == N:
+#         return maxSum
+#
+#     if idx == 0:
+#         findMax(depth+1, arr[depth+1][idx], idx)
+#
+#
+# num = max(arr[0])
+# maxSum += num
+# idx = arr[0].index(num)
+# findMax(0, idx)
+#
+# print(maxSum, minSum)
---
+++ 스터디/백준/0. 유형 모르고 풀기/6198. 옥상 정원 꾸미기.py
@@ -0,0 +1,34 @@
+N = int(input())
+
+building = []
+for _ in range(N):
+    building.append(int(input()))
+
+stack = []
+result = 0
+
+for b in building:
+    while stack and stack[-1] <=b:
+        stack.pop()
+    stack.append(b)
+    result += len(stack) - 1  # result에 더해주는 값은 i번째 건물을 볼 수 있는 다른 건물의 수
+
+print(result)
+
+
+
+
+# ans = [0] * N
+#
+# for i in range(N):
+#     cnt = 0
+#     for j in range(i+1, N):
+#         if building[i] <= building[j]:
+#             ans[i] = cnt
+#             break
+#         cnt += 1
+#         if j == N-1:
+#             ans[i] = cnt
+#
+# # print(ans)
+# print(sum(ans))
